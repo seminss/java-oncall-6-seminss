a9f8a14885213d7e5935b766d9ea7d55169e897f
seminss <2007ksm@naver.com>
2023-12-16 14:38:20 +0900

    feat: 요일, 월 정보를 담은 객체 생성

---
+++ src/main/java/oncall/model/EndOfMonth.java
@@ -0,0 +1,44 @@
+package oncall.model;
+
+import oncall.exception.OncallException;
+
+public enum EndOfMonth {
+    JANUARY(1, 31),
+    FEBRUARY(2, 28),
+    MARCH(3, 31),
+    APRIL(4, 30),
+    MAY(5, 31),
+    JUNE(6, 30),
+    JULY(7, 31),
+    AUGUST(8, 31),
+    SEPTEMBER(9, 30),
+    OCTOBER(10, 31),
+    NOVEMBER(11, 30),
+    DECEMBER(12, 31);
+
+    private final int month;
+
+    private final int endDay;
+
+    EndOfMonth(int month, int endDay) {
+        this.month = month;
+        this.endDay = endDay;
+    }
+
+    public static EndOfMonth of(int month) {
+        for (EndOfMonth endOfMonth : values()) {
+            if (endOfMonth.month == month) {
+                return endOfMonth;
+            }
+        }
+        throw new OncallException("해당하는 월이 없습니다.");
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getEndDay() {
+        return endDay;
+    }
+}
---
+++ src/main/java/oncall/model/Week.java
@@ -0,0 +1,33 @@
+package oncall.model;
+
+import oncall.exception.OncallException;
+
+public enum Week {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+
+    private final String dayOfWeek;
+
+    Week(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public static Week from(String koreanOfDayOfWeek) {
+        for (Week dayOfWeek : values()) {
+            if (dayOfWeek.dayOfWeek.equals(koreanOfDayOfWeek)) {
+                return dayOfWeek;
+            }
+        }
+        throw new OncallException("요일을 잘못 입력하셨습니다.");
+    }
+
+    public static Week next(Week dayOfWeek) {
+        int nextDayOfWeekIndex = (dayOfWeek.ordinal() + 1) % 7;
+        return values()[nextDayOfWeekIndex];
+    }
+}
